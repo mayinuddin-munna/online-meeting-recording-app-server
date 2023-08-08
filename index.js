5530dddfcf0c17d6cc133f322c2a266ec2f422e3
ofwebdev <nkomorfaruk@gmail.com>
2023-08-08 21:58:39 +0600

    Add initial set up

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const app = express()
+const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// ------------------
+// Express App
+// ------------------
+const app = express();
+
+// ------------------
+// Middleware
+// ------------------
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Online meeting recording app listening on port ${port}`)
-})
+  console.log(`Online meeting recording app listening on port ${port}`);
+});
