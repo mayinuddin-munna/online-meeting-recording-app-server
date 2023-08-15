9d43737550771322f87b527f49c15af4eb60b74a
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-08-15 13:09:25 +0600

    express setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# online-meeting-recording-app-server" 
+
+Vercel live link: https://galaxy-meeting.vercel.app/
--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.jkwo6ss.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Online meeting recording app listening on port ${port}`)
-})
+  console.log(`Online meeting recording app listening on port ${port}`);
+});
