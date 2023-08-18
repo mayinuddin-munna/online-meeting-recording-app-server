05400b8bb3fc9333691b21ca3ab0c262ddb04bba
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-08-18 18:06:47 +0600

    video features add

--- index.js
+++ index.js
@@ -1,115 +1,119 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const port = process.env.PORT || 5000;
+const socketIO = require("socket.io");
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+const port = process.env.PORT || 8080;
+const env = process.env.NODE_ENV || "development";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Redirect to https
+app.get("*", (req, res, next) => {
+  if (req.headers["x-forwarded-proto"] !== "https" && env !== "development") {
+    return res.redirect(["https://", req.get("Host"), req.url].join(""));
+  }
+  next();
 });
 
-// Verify JWT
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
-const verifyJWT = (req, res, next) => {
-  const authorization = req.headers.authorization;
+const server = require("http").createServer(app);
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
 
-  if (!authorization) {
-    return res.status(401).send({ error: "Unauthorized access!" });
+/**
+ * Socket.io events
+ */
+const io = socketIO(server);
+io.sockets.on("connection", function (socket) {
+  /**
+   * Log actions to the client
+   */
+  function log() {
+    const array = ["Server:"];
+    array.push.apply(array, arguments);
+    socket.emit("log", array);
   }
 
-  const token = authorization.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+  /**
+   * Handle message from a client
+   * If toId is provided message will be sent ONLY to the client with that id
+   * If toId is NOT provided and room IS provided message will be broadcast to that room
+   * If NONE is provided message will be sent to all clients
+   */
+  socket.on("message", (message, toId = null, room = null) => {
+    log("Client " + socket.id + " said: ", message);
 
-    if (error) {
-      return res.status(403).send({ error: "Unauthorized access!" })
+    if (toId) {
+      console.log("From ", socket.id, " to ", toId, message.type);
+
+      io.to(toId).emit("message", message, socket.id);
+    } else if (room) {
+      console.log("From ", socket.id, " to room: ", room, message.type);
+
+      socket.broadcast.to(room).emit("message", message, socket.id);
+    } else {
+      console.log("From ", socket.id, " to everyone ", message.type);
+
+      socket.broadcast.emit("message", message, socket.id);
     }
+  });
 
-    req.decoded = decoded;
-    next();
-  })
-}
+  let roomAdmin; // save admins socket id (will get overwritten if new room gets created)
 
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.jkwo6ss.mongodb.net/?retryWrites=true&w=majority`;
+  /**
+   * When room gets created or someone joins it
+   */
+  socket.on("create or join", (room) => {
+    log("Create or Join room: " + room);
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+    // Get number of clients in the room
+    const clientsInRoom = io.sockets.adapter.rooms.get(room);
+    let numClients = clientsInRoom ? clientsInRoom.size : 0;
 
-async function run() {
-  try {
-    await client.connect();
+    if (numClients === 0) {
+      // Create room
+      socket.join(room);
+      roomAdmin = socket.id;
+      socket.emit("created", room, socket.id);
+    } else {
+      log("Client " + socket.id + " joined room " + room);
 
-    // Galaxy collection
-    const usersCollection = client.db("galaxyMeeting").collection("users");
+      // Join room
+      io.sockets.in(room).emit("join", room); // Notify users in room
+      socket.join(room);
+      io.to(socket.id).emit("joined", room, socket.id); // Notify client that they joined a room
+      io.sockets.in(room).emit("ready", socket.id); // Room is ready for creating connections
+    }
+  });
 
+  /**
+   * Kick participant from a call
+   */
+  socket.on("kickout", (socketId, room) => {
+    if (socket.id === roomAdmin) {
+      socket.broadcast.emit("kickout", socketId);
+      io.sockets.sockets.get(socketId).leave(room);
+    } else {
+      console.log("not an admin");
+    }
+  });
 
-    // User related API
+  // participant leaves room
+  socket.on("leave room", (room) => {
+    socket.leave(room);
+    socket.emit("left room", room);
+    socket.broadcast.to(room).emit("message", { type: "leave" }, socket.id);
+  });
 
-    // TODO: add verifyJWT in the API
-    app.get('/all-users', async (req, res) => {
-      const result = await usersCollection.find().toArray();
-      res.send(result);
-    })
-
-    // TODO: add verifyJWT in the API
-    app.get('/user/:email', async(req, res) =>{
-      const email = req.params.email;
-      const decodedEmail = req.decoded.email;
-
-      // if(email !== decodedEmail){
-      //   return res.status(403).send({ error: 'forbidden access'})
-      // }
-      const query = { email : email };
-      const result = await usersCollection.findOne(query);
-    
-      res.send(result);
-
-    })
-
-    app.post('/add-users', async (req, res) => {
-      const user = req.body;
-      const query = { email: user.email };
-      const existingUser = await usersCollection.findOne(query)
-
-      if (existingUser) {
-        return res.send({ message: "User already exist" })
-      }
-
-      console.log("user", user);
-
-      const result = await usersCollection.insertOne(user);
-      res.send(result);
-    })
-
-    // JWT related api
-    app.post('/jwt', async (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '10h'
-      });
-      res.send({ token });
-    })
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
-app.listen(port, () => {
-  console.log(`Online meeting recording app listening on port ${port}`);
+  /**
+   * When participant leaves notify other participants
+   */
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => {
+      if (room === socket.id) return;
+      socket.broadcast.to(room).emit("message", { type: "leave" }, socket.id);
+    });
+  });
 });
